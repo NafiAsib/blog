4d24ee875c3c02dc35cd4e9c51fcbfe28fd0d22b
NafiAsib <nafi.asib@gmail.com>
2023-08-05 19:30:13 +0600

    feat: proper page metadata

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -1,7 +1,7 @@
-import Head from "next/head";
 import Link from "next/link";
 import { format, parseISO } from "date-fns";
 import { allPosts } from "contentlayer/generated";
+import type { Metadata } from "next";
 
 export async function generateStaticParams() {
   return allPosts.map((post) => ({
@@ -9,33 +9,39 @@ export async function generateStaticParams() {
   }));
 }
 
+type Props = {
+  [param: string]: any;
+};
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  return {
+    title: params.slug,
+  };
+}
+
 const PostLayout = ({ params }) => {
   const post = allPosts.find((post) => post._raw.flattenedPath === params.slug);
+  if (!post) throw new Error(`Cannot find post with slug ${params.slug}`);
   return (
-    <>
-      <Head>
-        <title>{post.title}</title>
-      </Head>
-      <article className="mx-auto max-w-2xl">
-        <div className="mb-6 text-center">
-          <Link href="/posts">
-            <span className="text-center text-sm font-bold uppercase text-slate-100">
-              Back to all notes
-            </span>
-          </Link>
-        </div>
-        <div className="mb-6 text-center">
-          <h1 className="mb-1 text-3xl font-bold">{post.title}</h1>
-          <time dateTime={post.date} className="text-sm text-slate-600">
-            {format(parseISO(post.date), "LLLL d, yyyy")}
-          </time>
-        </div>
-        <div
-          className="cl-post-body"
-          dangerouslySetInnerHTML={{ __html: post.body.html }}
-        />
-      </article>
-    </>
+    <article className="mx-auto max-w-2xl">
+      <div className="mb-6 text-center">
+        <Link href="/posts">
+          <span className="text-center text-sm font-bold uppercase text-slate-100">
+            Back to all notes
+          </span>
+        </Link>
+      </div>
+      <div className="mb-6 text-center">
+        <h1 className="mb-1 text-3xl font-bold">{post.title}</h1>
+        <time dateTime={post.date} className="text-sm text-slate-600">
+          {format(parseISO(post.date), "LLLL d, yyyy")}
+        </time>
+      </div>
+      <div
+        className="cl-post-body"
+        dangerouslySetInnerHTML={{ __html: post.body.html }}
+      />
+    </article>
   );
 };
 
