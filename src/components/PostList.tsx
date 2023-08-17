983da50e21b8f4549a8098d5d69d91140f2e2716
NafiAsib <nafi.asib@gmail.com>
2023-08-18 04:11:10 +0600

    chore: change date to publishedAt

--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -1,13 +1,21 @@
 import { compareDesc } from "date-fns";
 import { allPosts } from "contentlayer/generated";
 import PostPreview from "@/components/PostPreview";
+import Link from "next/link";
 
 export default function PostList() {
   return (
     <div className="flex flex-col gap-y-6">
+      <div className="mb-6 text-center">
+        <Link href="/">
+          <span className="text-center text-sm font-bold uppercase text-slate-100">
+            Back to home
+          </span>
+        </Link>
+      </div>
       {allPosts
         .sort((a, b) => {
-          return compareDesc(new Date(a.date), new Date(b.date));
+          return compareDesc(new Date(a.publishedAt), new Date(b.publishedAt));
         })
         .map((post, idx) => (
           <PostPreview key={idx} post={post} />
--- src/components/PostPreview.tsx
+++ src/components/PostPreview.tsx
@@ -11,19 +11,19 @@ type Props = {
 export default function PostPreview({ post }: Props) {
   return (
     <Link
-      href={post.url}
+      href={`posts/${post.slug}`}
       className={cn(
         "text-left block overflow-hidden rounded-2xl bg-white/5 p-7 shadow-surface-elevation-low transition duration-300 hover:bg-white/10 hover:shadow-surface-elevation-medium"
       )}
     >
       <h2 className="text-xl font-bold text-zinc-300">{post.title}</h2>
       <time
-        dateTime={post.date}
+        dateTime={post.publishedAt}
         className="block text-sm font-semibold text-slate-100"
       >
-        {format(parseISO(post.date), "MMMM d")}
+        {format(parseISO(post.publishedAt), "MMMM d")}
       </time>
-      <p className="mt-4">{post.description}</p>
+      <p className="mt-4 text-slate-300">{post.description}</p>
     </Link>
   );
 }
