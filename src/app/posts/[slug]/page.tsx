f7d73160e4148c59f491d2f716fc1474799181c4
NafiAsib <nafi.asib@gmail.com>
2023-08-18 04:12:24 +0600

    feat: metadata, date & typography updates

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -7,7 +7,7 @@ import { AnimatePage } from "@/components/atoms/AnimatePage";
 
 export async function generateStaticParams() {
   return allPosts.map((post) => ({
-    url: post.url,
+    url: post.slug,
   }));
 }
 
@@ -15,19 +15,58 @@ type Props = {
   [param: string]: any;
 };
 
-export async function generateMetadata({ params }: Props): Promise<Metadata> {
+export async function generateMetadata({
+  params,
+}: Props): Promise<Metadata | undefined> {
+  const post = allPosts.find((post) => post.slug === params.slug);
+
+  if (!post) {
+    return;
+  }
+  const { title, publishedAt, description } = post;
+
   return {
-    title: params.slug,
+    title,
+    description,
   };
 }
 
+function formatDate(date: string) {
+  const currentDate = new Date();
+  const targetDate = new Date(date);
+
+  const yearsAgo = currentDate.getFullYear() - targetDate.getFullYear();
+  const monthsAgo = currentDate.getMonth() - targetDate.getMonth();
+  const daysAgo = currentDate.getDate() - targetDate.getDate();
+
+  let formattedDate = "";
+
+  if (yearsAgo > 0) {
+    formattedDate = `${yearsAgo}y ago`;
+  } else if (monthsAgo > 0) {
+    formattedDate = `${monthsAgo}mo ago`;
+  } else if (daysAgo > 0) {
+    formattedDate = `${daysAgo}d ago`;
+  } else {
+    formattedDate = "Today";
+  }
+
+  const fullDate = targetDate.toLocaleString("en-us", {
+    month: "long",
+    day: "numeric",
+    year: "numeric",
+  });
+
+  return `${fullDate} (${formattedDate})`;
+}
+
 const PostLayout = ({ params }) => {
-  const post = allPosts.find((post) => post._raw.flattenedPath === params.slug);
+  const post = allPosts.find((post) => post.slug === params.slug);
   if (!post) throw new Error(`Cannot find post with slug ${params.slug}`);
   const MDXContent = getMDXComponent(post.body.code);
   return (
     <AnimatePage>
-      <article className="mx-auto max-w-2xl prose">
+      <article className="prose prose-headings:text-text text-left text-[#D4D4D4] prose-a:text-neutral-50 prose-strong:text-[#D4D4D4]">
         <div className="mb-6 text-center">
           <Link href="/posts">
             <span className="text-center text-sm font-bold uppercase text-slate-100">
@@ -36,9 +75,10 @@ const PostLayout = ({ params }) => {
           </Link>
         </div>
         <div className="mb-6 text-center">
-          <h1 className="mb-1 text-3xl font-bold">{post.title}</h1>
-          <time dateTime={post.date} className="text-sm text-slate-600">
-            {format(parseISO(post.date), "LLLL d, yyyy")}
+          <h1 className="mb-1 text-2xl font-bold">{post.title}</h1>
+          <time dateTime={post.publishedAt} className="text-sm text-slate-400">
+            {formatDate(post.publishedAt)}
+            {/* {format(parseISO(post.publishedAt), "LLLL d, yyyy")} */}
           </time>
         </div>
         <MDXContent />
