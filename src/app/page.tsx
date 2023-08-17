9fb5a5024cd37fd4014a5649e447adb8556c12ef
NafiAsib <nafi.asib@gmail.com>
2023-08-18 04:10:21 +0600

    chore: remove revalidate

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
 import PostList from "@/components/PostList";
 
-export const revalidate = 60;
-
 export default async function HomePage() {
   return (
     <section className="">
@@ -13,7 +11,7 @@ export default async function HomePage() {
         <p>Software Engineer from Dhaka, Bangladesh</p>
       </div>
 
-      <div className="mb-5  text-2xl bg-[radial-gradient(ellipse_at_top_right,_var(--tw-gradient-stops))] from-slate-100 via-slate-300 to-slate-400 bg-clip-text text-transparent">
+      <div className="mb-5 text-xl bg-[radial-gradient(ellipse_at_top_right,_var(--tw-gradient-stops))] from-slate-100 via-slate-300 to-slate-400 bg-clip-text text-transparent">
         <p>This an experimental digital garden where I share my learnings!</p>
       </div>
       <PostList />
