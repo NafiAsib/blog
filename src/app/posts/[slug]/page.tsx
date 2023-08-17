24e164a7206ffdd39ca29a59659871a7d33e1ba4
NafiAsib <nafi.asib@gmail.com>
2023-08-17 16:07:33 +0600

    feat: add page animation

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import { getMDXComponent } from "next-contentlayer/hooks";
 import { format, parseISO } from "date-fns";
 import { allPosts } from "contentlayer/generated";
+import { AnimatePage } from "@/components/atoms/AnimatePage";
 
 export async function generateStaticParams() {
   return allPosts.map((post) => ({
@@ -25,26 +26,24 @@ const PostLayout = ({ params }) => {
   if (!post) throw new Error(`Cannot find post with slug ${params.slug}`);
   const MDXContent = getMDXComponent(post.body.code);
   return (
-    <article className="mx-auto max-w-2xl">
-      <div className="mb-6 text-center">
-        <Link href="/posts">
-          <span className="text-center text-sm font-bold uppercase text-slate-100">
-            Back to all notes
-          </span>
-        </Link>
-      </div>
-      <div className="mb-6 text-center">
-        <h1 className="mb-1 text-3xl font-bold">{post.title}</h1>
-        <time dateTime={post.date} className="text-sm text-slate-600">
-          {format(parseISO(post.date), "LLLL d, yyyy")}
-        </time>
-      </div>
-      <MDXContent />
-      {/* <div
-        className="cl-post-body"
-        dangerouslySetInnerHTML={{ __html: post.body.html }}
-      /> */}
-    </article>
+    <AnimatePage>
+      <article className="mx-auto max-w-2xl">
+        <div className="mb-6 text-center">
+          <Link href="/posts">
+            <span className="text-center text-sm font-bold uppercase text-slate-100">
+              Back to all notes
+            </span>
+          </Link>
+        </div>
+        <div className="mb-6 text-center">
+          <h1 className="mb-1 text-3xl font-bold">{post.title}</h1>
+          <time dateTime={post.date} className="text-sm text-slate-600">
+            {format(parseISO(post.date), "LLLL d, yyyy")}
+          </time>
+        </div>
+        <MDXContent />
+      </article>
+    </AnimatePage>
   );
 };
 
