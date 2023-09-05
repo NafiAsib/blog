7ae57a28857ad38a6163b2ea7a58863f06cb8211
NafiAsib <nafi.asib@gmail.com>
2023-09-05 23:58:40 +0600

    fix: conditional & markup

--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -4,9 +4,17 @@ import { Check, Warning } from "@/icons";
 export default function Alert(props) {
   return (
     <div className="relative w-full px-4 py-2 border border-neutral-600 rounded text-sm flex items-center">
-      <span className="m-0 mr-4">{props.type === "warn" && <Warning />}</span>
-      <span className="m-0 mr-4">{props.type === "check" && <Check />}</span>
-      <p className="m-0">{props.children}</p>
+      {props.type === "warn" && (
+        <span className="m-0 mr-4">
+          <Warning />
+        </span>
+      )}
+      {props.type === "check" && (
+        <span className="m-0 mr-4">
+          <Check />
+        </span>
+      )}
+      <span className="m-0">{props.children}</span>
     </div>
   );
 }
